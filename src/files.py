cd4edf030567f1874d4895c1344d2ef4e6910010
Aristy <axelmezade@orange.fr>
2023-12-10 18:32:29 +0000

    export to little functions

--- src/files.py
+++ src/files.py
@@ -5,36 +5,38 @@ import os # For getting path and navigating
 def getFiles(extension: str) -> list[str]:
 
     current_directory = os.getcwd()
-    print("Current Directory:", current_directory)
+    print("Project Directory:", current_directory)
 
     # Define the path
     target_directory = os.path.join(current_directory, "app", "src", "main", "java")
-    print("Target Directory :", target_directory)
+    #print("Target Directory :", target_directory)
 
-    while True:
-        # List directories in the current directory
-        items = 0
-        subdirectory = ""
-        for item in os.listdir(target_directory):
-            item_path = os.path.join(target_directory, item)
-            if os.path.isdir(item_path):
-                subdirectory = item
-            items += 1
-
-        print(f"Directory: {target_directory} {target_directory}")
-
-        # If there is only one subdirectory, navigate into it
-        if items == 1:
-            target_directory = os.path.join(target_directory, subdirectory)
-        else:
-            # If there are no subdirectories or more than one, break out of the loop
-            break
+    navigateToFinalDir(target_directory)
 
     return listFiles(target_directory, extension)
 
     
+def navigateToFinalDir(source_dir: str):
+    while True:
+        # List directories in the current directory
+        items = 0
+        subdirectory = ""
+        for item in os.listdir(source_dir):
+            item_path = os.path.join(source_dir, item)
+            if os.path.isdir(item_path):
+                subdirectory = item
+            items += 1
 
+        #print(f"Directory: {source_dir}")
 
+        # If there is only one subdirectory, navigate into it
+        if items == 1:
+            source_dir = os.path.join(source_dir, subdirectory)
+        else:
+            # If there are no subdirectories or more than one, break out of the loop
+            break
+                
+            
 def listFiles(directory: str, extension: str) -> list[str]:
     items = []
     for root, dirs, files in os.walk(directory):
