a5708ed9d498e6c865ad5ea568c408e69f5795f2
Aristy <axelmezade@orange.fr>
2023-12-11 22:07:52 +0000

    Generate latex for all the file

--- src/dataFile.py
+++ src/dataFile.py
@@ -4,11 +4,15 @@ class DataFile:
     def __init__(self) -> None:
         self.fileName = ""
         self.extension = ""
-        self.description = ""
-        self.functions = [Function]
+        #self.type = "" # class, file, class activity, enum, ...
+        self.__description = ""
+        self.__hasDescription = False
+        self.functions = []
 
         self.__rawData = []
         pass
+
+    # -- Setters --
 
     def setFilename(self, fileName: str):
         self.fileName = fileName
@@ -16,8 +20,33 @@ class DataFile:
     def setExtension(self, extension: str):
         self.extension = extension
 
+    def setDescription(self, description: str):
+        self.__description = description
+        self.__hasDescription = True
+
     def addRawData(self, comment: str, function: str):
         self.__rawData.append([comment, function])
 
+    def addFunction(self, function: Function):
+        self.functions.append(function)
+
+    # -- Getters --
+
+    def getFileName(self) -> str:
+        return self.fileName
+    
+    def getDescription(self) -> str:
+        return self.__description
+
+    def hasDescription(self) -> bool:
+        return self.__hasDescription
+
     def getRawData(self):
         return self.__rawData
+    
+    def getFunctions(self) -> list[Function]:
+        return self.functions
+    
+
+    def __str__(self) -> str:
+        return f"{self.fileName}{self.extension} - {self.functions}"
--- src/generate.py
+++ src/generate.py
@@ -1,22 +1,37 @@
-from .function import Function
+from src.function import Function
+from src.dataFile import DataFile
 
-def generateLatexFunction(function: Function, filename: str):
-    latex_output = f"\\begin{{func}}{{{filename}.{function.getFonctionDec()}}}\n"
+def generateLatex(dataFile: DataFile) -> str:
+    latexOutput =  f"\section{{{dataFile.getFileName()}}}\n\n"
+
+    # Explain the file if there is a description
+    if dataFile.hasDescription():
+        latexOutput += f"{dataFile.getDescription()}\n\n"
+    
+    # Populate functions
+    for function in dataFile.getFunctions():
+        latexFunction = generateLatexFunction(function, dataFile.fileName)
+        latexOutput += f"{latexFunction}\n\n"
+
+    return latexOutput
+
+def generateLatexFunction(function: Function, filename: str) -> str:
+    latexOutput = f"\\begin{{func}}{{{filename}.{function.getFonctionDec()}}}\n"
 
     if function.description:
-        latex_output += f"    \\item {function.description}\n"
+        latexOutput += f"    \\item {function.description}\n"
 
     if function.parameters:
         for param_name, param_description in function.parameters:
-            latex_output += f"    \\item \\param{{{param_name}}}{{{param_description}}}\n"
+            latexOutput += f"    \\item \\param{{{param_name}}}{{{param_description}}}\n"
     
     if function.interruptions:
         for interruption_name, int_description in function.interruptions:
-            latex_output += f"    \\item \\inter{{{interruption_name}}}{{{int_description}}}\n"
+            latexOutput += f"    \\item \\inter{{{interruption_name}}}{{{int_description}}}\n"
 
     if function.returns:
         for ret_description in function.returns:
-            latex_output += f"    \\item \\ret{{{ret_description}}}\n"
+            latexOutput += f"    \\item \\ret{{{ret_description}}}\n"
 
-    latex_output += "\\end{func}\n"
-    print(latex_output)
+    latexOutput += "\\end{func}\n"
+    return latexOutput
