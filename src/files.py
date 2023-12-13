15a2351fd9700289e20c46c9584a382ded276ef1
Aristy <axelmezade@orange.fr>
2023-12-13 10:47:17 +0000

    differentiate between .kt and .py files an if there is an android project

--- src/files.py
+++ src/files.py
@@ -1,20 +1,66 @@
 import os # For getting path and navigating
+from collections import Counter
 
 #[project name]/app/src/main/java/[com/example]/[.kt and packages]
 
-def getFiles(extension: str) -> tuple[list[str], str]:
-
+def getFiles() -> tuple[list[str], str]:
     current_directory = os.getcwd()
     print("Project Directory:", current_directory)
 
+    # Detect project extension used
+    extension = primaryExtension(current_directory)
+    print(f"Common extension: {extension}")
+
     # Define the path
-    target_directory = os.path.join(current_directory, "app", "src", "main", "java")
+    target_directory = getTargetDirectory(current_directory, extension)
     #print("Target Directory :", target_directory)
 
     navigateToFinalDir(target_directory)
 
     return listFiles(target_directory, extension), os.path.basename(current_directory)
 
+
+def primaryExtension(path: str) -> str:
+    extension_supported = [".kt", ".py"]
+    file_extensions = []
+
+    # Traverse the directory and its subdirectories
+    for _, _, files in os.walk(path):
+        for file in files:
+            _, extension = os.path.splitext(file)
+            # Check if the extension is in the allowed list
+            if extension.lower() in extension_supported:
+                file_extensions.append(extension.lower())
+
+    # Count the occurrences of each file extension
+    extension_counter = Counter(file_extensions)
+
+    # Find the most common file extension
+    most_common_extension = extension_counter.most_common(1)
+
+    if most_common_extension:
+        return most_common_extension[0][0]
+    else:
+        return ""
+    
+def getTargetDirectory(root_path: str, extension: str) -> str:
+    target_directory = root_path
+    # if .kt verify if its not a android project
+    if extension == ".kt": # or extension == ".java":
+        if isAndroidProject(root_path):
+            target_directory = os.path.join(root_path, "app", "src", "main", "java")
+    
+    return target_directory
+
+def isAndroidProject(root_path: str) -> bool:
+    android_project_dir = ["app", "build", "gradle"]
+
+    for directory in android_project_dir:
+        directory_path = os.path.join(root_path, directory)
+        if not os.path.exists(directory_path) or not os.path.isdir(directory_path):
+            return False
+
+    return True
     
 def navigateToFinalDir(source_dir: str):
     while True:
