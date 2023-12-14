52d8c24205bd371d201f7d490b30841e3fe1e7da
Aristy <axelmezade@orange.fr>
2023-12-14 19:00:24 +0000

    Extract comment and declaration function in python

--- src/extract.py
+++ src/extract.py
@@ -17,9 +17,47 @@ def extractData(file_path: str) -> DataFile:
 
     if datafile.getExtension() == ".kt":
         extractKotlin(file_path, datafile)
+    elif datafile.getExtension() == ".py":
+        extractPython(file_path, datafile)
 
     return datafile
     
+def extractPython(filePath: str, datafile: DataFile):
+    with open(filePath, 'r') as file:
+
+        maybe_comment = False
+        in_comment = False
+        current_comment = ""
+
+        current_function = ""
+
+        format_function = False
+
+        for line in file:
+            formLine = line.strip()
+
+            if formLine.startswith('# ') and not maybe_comment:
+                maybe_comment = True
+                current_comment = formLine + "\n"
+            elif formLine == "":
+                maybe_comment = False
+                current_comment = "" # Clean comment
+            elif formLine.startswith('# @') and maybe_comment:
+                in_comment = True
+                current_comment += formLine + "\n"
+
+            # End of the comment, check the function
+            elif not formLine.startswith('# ') and in_comment:
+                #print(current_comment)
+                in_comment = False
+                format_function = True
+                current_function = formLine
+            
+            if format_function:
+                datafile.addRawData(current_comment, current_function)
+                current_comment = ""
+                current_function = ""
+                format_function = False
 
 def extractKotlin(filePath: str, datafile: DataFile):
     with open(filePath, 'r') as file:
